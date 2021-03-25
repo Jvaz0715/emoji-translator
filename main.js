242c2d6364dfe978426d2af99901ad98339caa22
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-25 14:20:40 -0400

    adds encode

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //first we need to query  our input box with # translator-input
-const input = document.querySelector('#translator-input');
+const input = document.querySelector("#translator-input");
 
 //once we queried the element  grab its .value property containing the inputs contents and store it in a variable
-
 const inputValue = input.value;
 
 
+// query the h1 with #results where we put our output to our user
+const result = document.querySelector("#results");
+
+//set its innerText to the result of passing the input 
+result.innerText = encode(input.value);
