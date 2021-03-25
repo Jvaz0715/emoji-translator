5d7833f1a5b66e344fbbd924575957bea8c088b1
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-25 14:00:19 -0400

    queries #translator-input and creates a variable to hold its  .value

---
+++ main.js
@@ -0,0 +1,8 @@
+//first we need to query  our input box with # translator-input
+const input = document.querySelector('#translator-input');
+
+//once we queried the element  grab its .value property containing the inputs contents and store it in a variable
+
+const inputValue = input.value;
+
+
