3c70b3f255b967f80bdd654dbda4e1a4188e8737
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-25 15:30:58 -0400

    solves first feature

--- main.js
+++ main.js
@@ -2,11 +2,17 @@
 const input = document.querySelector("#translator-input");
 
 //once we queried the element  grab its .value property containing the inputs contents and store it in a variable
-const inputValue = input.value;
-
+//const inputValue = input.value;
 
 // query the h1 with #results where we put our output to our user
 const result = document.querySelector("#results");
 
-//set its innerText to the result of passing the input 
-result.innerText = encode(input.value);
+// query the submit button and then add event listener
+const submitButton = document.querySelector('#submit-button');
+
+//now add event listener
+
+submitButton.addEventListener('click', function() {
+    const inputValue = input.value;
+    results.innerText = encode(inputValue);
+})
