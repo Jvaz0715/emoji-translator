12b8657073b855fab41879e817058985904c77cb
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-26 13:49:04 -0400

    Solves random function

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const submitButton = document.querySelector('#submit-button');
 
 submitButton.addEventListener('click', function() {
     const inputValue = input.value;
-    //results.innerText = encode(inputValue);
+    const randomNumber = randomElement([0, 1, 2, 3]);
 
     //determine which radio is pressed;
     const radios = document.querySelectorAll('[type="radio"]');
@@ -35,21 +35,46 @@ submitButton.addEventListener('click', function() {
             const array = search(inputValue);
             //console.log(array);
             result.innerHTML = '';
-            //handles an empty array
             if (array.length === 0) {
                 results.innerText = "No emojis found";
-            } 
-            //handles array with emojis
-            else {
+            } else {
                 for (let i = 0; i < array.length; i++) {
                     //console.log(array[i].symbol);
                     let emoji = array[i].symbol;
-                    const para = document.createElement("P");
-                    para.InnerText = emoji;
-                    results.appendChild(para);
+                    results.innerText += emoji;
                 }
-            }
-            
+            } 
+        }
+
+        if (radio.checked === true && radio.value === "random" && randomNumber === 0) {
+            console.log(randomNumber);
+            results.innerText = encode(inputValue);
+        }
+
+        if (radio.checked === true && radio.value === "random" && randomNumber === 1) {
+            console.log(randomNumber);
+            results.innerText = madlib(inputValue);
+        }
+
+        if (radio.checked === true && radio.value === "random" && randomNumber === 2) {
+            console.log(randomNumber);
+            results.innerText = translate(inputValue);
+        }
+
+        if (radio.checked === true && radio.value === "random" && randomNumber === 3) {
+            console.log(randomNumber);
+            const array = search(inputValue);
+            //console.log(array);
+            result.innerHTML = '';
+            if (array.length === 0) {
+                results.innerText = "No emojis found";
+            } else {
+                for (let i = 0; i < array.length; i++) {
+                    //console.log(array[i].symbol);
+                    let emoji = array[i].symbol;
+                    results.innerText += emoji;
+                }
+            } 
         }
     }
 })
